4000c4b5a9905ad6031c01aab29c1b87ca5b8c22
Ihor_Fomin <ihor.fomin@gmail.com>
2025-07-09 10:22:56 +0300

    Final commit, fix bug and improved functionality

--- orders/models.py
+++ orders/models.py
@@ -15,6 +15,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    stripe_id = models.CharField(max_length=250, blank=True)
 
 
     class Meta:
@@ -30,6 +31,16 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            return ''
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path = '/'
+        return f'https://dashboard.stripe.com{path}payment/{self.stripe_id}'
     
 
 class OrderItem(models.Model):
